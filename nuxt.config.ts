4819433f78c78d87f52ffe142a71aa0b6e656d37
Michael Bolliger <michael.bolliger@propertycaptain.ch>
2023-04-14 09:16:54 +0200

    test function

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,17 +3,25 @@ export default defineNuxtConfig({
   experimental: {
     noVueServer: false // Is set to true in the copyDataModule
   },
-  routeRules: {
-    '/api/**': {
-      security: {
-        corsHandler: {
-          origin: [
-            /https:\/\/[\w-]*.pensly.ch/,
-            /https:\/\/pensly.vercel.app/,
-            /http:\/\/localhost:\d+/,
-            /https:\/\/[\w-]*devbrains-com.vercel.app/
-          ]
-        }
+  security: {
+    corsHandler: {
+      origin: (origin: string) => {
+        const origins = [
+          /https:\/\/[\w-]*.pensly.ch/,
+          /https:\/\/pensly.vercel.app/,
+          /http:\/\/localhost:\d+/,
+          /https:\/\/[\w-]*devbrains-com.vercel.app/
+        ];
+
+        return origins.some((_origin) => {
+          console.log('origin', _origin);
+          if (_origin instanceof RegExp) {
+            console.log('is regex match', _origin.test(origin));
+            return _origin.test(origin);
+          }
+
+          return origin === _origin;
+        });
       }
     }
   },
